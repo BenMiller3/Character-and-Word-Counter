0f0b2978da5422d617d670109d3345883c31e179
Ben Miller <benjaminmiller229@gmail.com>
2016-04-07 23:32:24 -0400

    Create counter.py

---
+++ counter.py
@@ -0,0 +1,18 @@
+"""
+Counts the number of distinct words
+within a text file
+By: Benjamin D. Miller
+"""
+def wordCounter(filename):
+    wordCount = 0
+    charCount = 0
+    file = open(filename,'r')
+    for line in file.readlines():
+        for i in range(len(line)):
+            if(line[i]==" "): wordCount+=1
+        charCount+=1
+    ans = "\tTotal words: " + str(wordCount) + "\n\tTotal characters: " + str(charCount)
+    return ans
+
+### Test Cases ###
+#print(wordCounter(argv[0]))
