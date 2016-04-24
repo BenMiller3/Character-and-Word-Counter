56275d35ead101d377b28ccaaabd0fc8a64338cd
Ben Miller <benjaminmiller229@gmail.com>
2016-04-24 02:28:25 -0400

    Update counter.py

--- counter.py
+++ counter.py
@@ -3,6 +3,8 @@ Counts the number of distinct words
 within a text file
 By: Benjamin D. Miller
 """
+import sys
+
 def wordCounter(filename):
     wordCount = 0
     charCount = 0
@@ -14,10 +16,19 @@ def wordCounter(filename):
     ans = "\tTotal words: " + str(wordCount) + "\n\tTotal characters: " + str(charCount)
     return ans
 
+"""
+Pass filename to be counted as the arguements
+Checks it is a valid filename and then counts
+"""
 def main():
-    filename = in.next()
-    wordCounter(filename)
-
+    if len(sys.arg) == 2:   # Argument passed should be filename
+        filename = sys.arg[1]
+    
+    try:
+        wordCounter(filename)
+    except:
+        print("Invalid file name")
+        
 if __name__ == '__main__':
     main()
     
